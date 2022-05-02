0ccb124573c1c62008fa66f1143ea5ff2ab0a74a
skillor <skillor@gmx.net>
2022-05-02 22:22:13 +0200

    Bump to version 0.0.1

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [x for x in fh.read().splitlines() if x]
+
+setup(name='Mime Codec',
+      packages=['mime_codec'],
+      version='0.0.1',
+      description='get mime codecs for media purposes',
+      author='skillor',
+      author_email='skillor@gmx.net',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      url='https://github.com/skillor/mime-codec',
+      keywords=['mime', 'codec', 'mime-codec', 'mediasource'],
+      classifiers=['Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.9',
+                   'Topic :: Internet'],
+      setup_requires=["wheel"],
+      install_requires=requirements,
+      python_requires='>=3',
+      )
