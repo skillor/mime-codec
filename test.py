1e21aa4fc223aba8f3237a11d40f505989d0918d
skillor <skillor@gmx.net>
2022-05-02 22:28:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import mime_codec
 
 
 class Tests(unittest.IsolatedAsyncioTestCase):
-    pass
+    async def test_mime_type_fallback(self):
+        self.assertEqual('fallback/fallback', mime_codec.get_mime_type('test', fallback='fallback/fallback'))
 
 
 if __name__ == '__main__':
