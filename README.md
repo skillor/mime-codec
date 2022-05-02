23e46af587f3d800225f3bab0f77bbdd1584bc2e
skillor <skillor@gmx.net>
2022-05-02 22:13:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# - Mime Codec -
+
+### get mime codecs specified by https://developer.mozilla.org/en-US/docs/Web/Media/Formats/codecs_parameter
---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+import mime_codec
+
+
+class Tests(unittest.IsolatedAsyncioTestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
