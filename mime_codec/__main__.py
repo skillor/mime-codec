fdeaaba7bacd280ca0f1b6de4580a708b4fc54e6
skillor <skillor@gmx.net>
2022-05-03 11:24:10 +0200

    feat: add recursive option

--- mime_codec/__main__.py
+++ mime_codec/__main__.py
@@ -1,5 +1,27 @@
-import sys
+import argparse
+import pathlib
+
+import os
 from .mime_codec import *
 
+
+def main():
+    parser = argparse.ArgumentParser(description='Mime Codec info')
+    parser.add_argument('path', type=pathlib.Path)
+    parser.add_argument('-r', '--recursive', action=argparse.BooleanOptionalAction, help='recursive folder')
+
+    args = parser.parse_args()
+    if args.recursive:
+        for dp, dn, filenames in os.walk(args.path):
+            for f in filenames:
+                p = os.path.join(dp, f)
+                try:
+                    print('{}: {}'.format(p, get_mime_codec(p)))
+                except:
+                    pass
+        return
+    print(get_mime_codec(args.path))
+
+
 if __name__ == '__main__':
-    print(get_mime_codec(sys.argv[1]))
+    main()
