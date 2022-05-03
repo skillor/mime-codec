24354f11dc393e68efd8987250d83846f181d194
skillor <skillor@gmx.net>
2022-05-03 10:36:10 +0200

    Revert "fix: change dependency"
    This reverts commit 36c4c8658286011b9b95a05c321e8b5b6df9a0d7.

--- mime_codec/mime_codec.py
+++ mime_codec/mime_codec.py
@@ -1,6 +1,5 @@
 import mimetypes
-from ffprobe import FFProbe
-from ffprobe.ffprobe import FFStream
+import ffmpeg
 
 from .profiles_avc import get_avc_profile
 from .exceptions import CodecException
@@ -15,18 +14,18 @@ def get_mime_type(file_path: str, fallback: str = 'video/mp4'):
     return mime_type
 
 
-def get_codec(probe_stream: FFStream, mime_type: str):
+def get_codec(probe_stream, mime_type: str):
     if mime_type in WEBM_MIME_TYPES:
-        return probe_stream.codec()
+        return probe_stream['codec_name']
 
-    if probe_stream.__dict__['codec_tag_string'] == 'avc1':
-        level = probe_stream.__dict__['level']
+    if probe_stream['codec_tag_string'] == 'avc1':
+        level = probe_stream['level']
         level = f'{int(level):02x}'
-        number, constraint = get_avc_profile(probe_stream.__dict__['profile'])
+        number, constraint = get_avc_profile(probe_stream['profile'])
         return 'avc1.{}{}{}'.format(number, constraint, level).lower()
 
-    if probe_stream.__dict__['codec_tag_string'] == 'mp4a':
-        return 'mp4a.40.{}'.format(get_mp4a_profile(probe_stream.__dict__['profile']))
+    if probe_stream['codec_tag_string'] == 'mp4a':
+        return 'mp4a.40.{}'.format(get_mp4a_profile(probe_stream['profile']))
 
     raise CodecException('Codec not found', probe_stream)
 
@@ -34,8 +33,8 @@ def get_codec(probe_stream: FFStream, mime_type: str):
 def get_codecs(file_path: str, mime_type: str = None):
     if mime_type is None:
         mime_type = get_mime_type(file_path)
-    metadata = FFProbe(file_path)
-    return list(map(lambda x: get_codec(x, mime_type), metadata.streams))
+    probe = ffmpeg.probe(file_path)
+    return list(map(lambda x: get_codec(x, mime_type), probe['streams']))
 
 
 def get_mime_codec(file_path: str):
